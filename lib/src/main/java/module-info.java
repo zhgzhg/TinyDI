8840430f06f086132d50ae72eb7dd3aa7c6dbe26
zhgzhg <zhgzhg@users.noreply.github.com>
2023-03-04 00:43:37 +0200

    Define a real JPMS module for the TinyDI lib

---
+++ lib/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+open module com.github.zhgzhg.tinydi.tinydi {
+    exports com.github.zhgzhg.tinydi;
+    exports com.github.zhgzhg.tinydi.build;
+    exports com.github.zhgzhg.tinydi.components;
+    exports com.github.zhgzhg.tinydi.dynamic;
+    exports com.github.zhgzhg.tinydi.meta;
+    exports com.github.zhgzhg.tinydi.meta.annotations;
+    exports com.github.zhgzhg.tinydi.meta.enums;
+
+    requires java.base;
+    requires static lombok;
+    requires transitive io.github.classgraph;
+}
